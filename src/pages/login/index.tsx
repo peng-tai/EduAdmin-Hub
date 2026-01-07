2f38d02b5b3eaafb91528cd63581428255500ceb
pengtai <1015298655@qq.com>
2026-01-07 15:04:39 +0800

    feat: update ui

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,20 +1,29 @@
 import React, { useState } from 'react';
 import styles from './index.module.scss';
 
+import { Button } from 'antd';
+
 const Login = () => {
   const [account, setAccount] = useState('');
+  const handleRegister = () => {
+    console.log('register');
+  };
 
   return (
     <div className={styles.container}>
       <div className={styles.loginBox}>
         {account}
         <div className={styles.leftBox}>
-          <div className={styles.leftHeader}>logo</div>
+          <div className={styles.leftHeader}>教育管理系统</div>
 
           <div className={styles.leftContent}>
             <div className={styles.welcome}>欢迎回来</div>
-            <div className={styles.platformInfo}>这是管理系统介绍，一个让更多人收益的教育平台</div>
-            <div className={styles.register}>去注册账号</div>
+            <div className={styles.platformInfo}>
+              这是管理系统介绍，一个让更多人收益的教育平台
+            </div>
+            <Button className={styles.register} onClick={handleRegister}>
+              没账号，去注册
+            </Button>
           </div>
         </div>
         <div className={styles.rightBox}></div>
